4d19eb9eb9c4e3f4555b9e3f6b8e8c7cb575bacc
klimekjan <janklimek2008@gmail.com>
2026-06-28 00:39:20 +0200

    Revert "Merge branch 'main' of https://github.com/klimekjan/Lod-wka---Hack4Change"
    This reverts commit da842904b772db0b9377b68b98e952f14dfc670d, reversing
    changes made to 61b2786cf821eb4d1d7793acf80b3d4258fc530e.

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,20 +1,6 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_API_URL?: string
-}
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
-
 import axios from 'axios'
 
-
-
-
-
-
-const api = axios.create({ baseURL: import.meta.env.VITE_API_URL ?? '/api' })
+const api = axios.create({ baseURL: '/api' })
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
